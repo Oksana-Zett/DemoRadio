8fce2435e64fa6f56f2820a7c932b40507f34cc3
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2021-12-20 12:37:27 +0300

    Added Radio

--- src/main/java/domain/Radio.java
+++ src/main/java/domain/Radio.java
@@ -1,4 +1,66 @@
 package domain;
 
 public class Radio {
+    private int currentRadioStation;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > 9) {
+            this.currentRadioStation = 0;
+        } else if (currentRadioStation < 0) {
+            this.currentRadioStation = 9;
+        } else {
+            this.currentRadioStation = currentRadioStation;
+        }
+    }
+
+    public void setNextRadioStation() {
+        int nextRadioStation = currentRadioStation + 1;
+        setCurrentRadioStation(nextRadioStation);
+    }
+
+    public void setPrevRadioStation() {
+        int prevRadioStation = currentRadioStation - 1;
+        setCurrentRadioStation(prevRadioStation);
+    }
+
+    public void setValidNumberRadioStation(int currentRadioStation) {
+        if (currentRadioStation > 9) {
+            System.out.println("Номер станции недопустим");
+        } else if (currentRadioStation < 0) {
+            System.out.println("Номер станции недопустим");
+        } else {
+            setCurrentRadioStation(currentRadioStation);
+        }
+    }
+
+    private int currentVolume;
+
+    public int getCurrentVolume() {
+
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > 10) {
+            this.currentVolume = 10;
+        } else if (currentVolume < 0) {
+            this.currentRadioStation = 0;
+        } else {
+            this.currentVolume = currentVolume;
+        }
+    }
+
+    public void setNextVolume() {
+        int nextVolume = currentVolume + 1;
+        setCurrentVolume(nextVolume);
+    }
+
+    public void setPrevVolume() {
+        int prevVolume = currentVolume - 1;
+        setCurrentVolume(prevVolume);
+    }
 }
